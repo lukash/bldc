f1e21fcb17c9adaab82692bac2d80e6418631026
Lukáš Hrázký <lukkash@email.cz>
2025-06-13 22:26:26 +0200

    Don't check for temperature faults if cutoff end >= 100
    Feature: Disable Motor/MOSFET temeperature fault if Temp Cutoff End is
     set to 100°C or above
     >
     Setting the Temp Cutoff End to 100°C or higher for either Motor or
     MOSFET will completely disable the VESC firmware logic for throttling
     and eventually raising a fault if the temperature is too high.
     >
     It is expected you rely on and respect the temperature alerts from the
     package.
     >
     This is not necessary under normal circumstances (the package alerts
     trigger before any throttling happens), but can save a nosedive in case
     of a hardware failure resulting in false thermistor readings of extreme
     temperatures (i.e. in case of the thermistor getting shorted).

--- motor/mc_interface.c
+++ motor/mc_interface.c
@@ -2334,6 +2334,10 @@ static void update_override_limits(volatile motor_if_state_t *motor, volatile mc
 	// Temperature MOSFET
 	float lo_min_mos = l_current_min_tmp;
 	float lo_max_mos = l_current_max_tmp;
+
+	// FET CUTOFF: Beware indentation, it was preserved in if body for easy audit
+	if (conf->l_temp_fet_end < 100.0f) {
+
 	if (motor->m_temp_fet < (conf->l_temp_fet_start + 0.1)) {
 		// Keep values
 	} else if (motor->m_temp_fet > (conf->l_temp_fet_end - 0.1)) {
@@ -2357,9 +2361,15 @@ static void update_override_limits(volatile motor_if_state_t *motor, volatile mc
 		}
 	}
 
+	} // FET CUTOFF end
+
 	// Temperature MOTOR
 	float lo_min_mot = l_current_min_tmp;
 	float lo_max_mot = l_current_max_tmp;
+
+	// MOTOR CUTOFF: Beware indentation, it was preserved in if body for easy audit
+	if (conf->l_temp_motor_end < 100.0f) {
+
 	if (motor->m_temp_motor < (conf->l_temp_motor_start + 0.1)) {
 		// Keep values
 	} else if (motor->m_temp_motor > (conf->l_temp_motor_end - 0.1)) {
@@ -2382,6 +2392,8 @@ static void update_override_limits(volatile motor_if_state_t *motor, volatile mc
 			lo_max_mot = SIGN(l_current_max_tmp) * maxc;
 		}
 	}
+
+	} // MOTOR CUTOFF end
 
 	// Decreased temperatures during acceleration
 	// in order to still have braking torque available
